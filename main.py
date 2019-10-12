5464a40d88c2b611e0bc638d4e46531c6d612c41
metariani <metarianiananda05@gmail.com>
2019-10-12 12:07:53 +0700

    kecepatan tanpa fungsi

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ print(f'Program {program} oleh {nama}')
 
 def hitung_tegangan(arus , hambatan):
     tegangan = arus * hambatan
-    print(f'arus = {arus / 30}A dengan hambatan = {hambatan / 8}ohm')
+    print(f'arus = {arus * 30}A dengan hambatan = {hambatan * 8}ohm')
     print(f'sehingga tegangan = {tegangan} volt')
     return arus * hambatan
 
 #arus = 30
 #hambatan = 8
 tegangan = hitung_tegangan(30, 8)
-tegangan = hitung_tegangan(50, 6)
+tegangan = hitung_tegangan(45, 6)
 
